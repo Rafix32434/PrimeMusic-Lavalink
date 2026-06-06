e3c2ac66dfd688c28d699b6ceec2dfa0955ff644
Rafix32434 <149138424+Rafix32434@users.noreply.github.com>
2026-06-06 19:08:41 +0200

    config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 
 module.exports = {
-  TOKEN: "",
+  TOKEN: "MTUxMjM2MTUzMDY2MDg4NDQ4MA.GgLt1a.HcxgPyO9tqmeIaqb2EuyjA9x1-M7MRn_zKzzDM",
   language: "en",
   ownerID: ["962994407651553302", ""], 
   mongodbUri : "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
