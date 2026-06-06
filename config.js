66d5efa9bafca0609c2ceaa19825244480a42a47
Rafix32434 <149138424+Rafix32434@users.noreply.github.com>
2026-06-06 19:17:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 
 
 module.exports = {
-  TOKEN: "MTUxMjM2MTUzMDY2MDg4NDQ4MA.GgLt1a.HcxgPyO9tqmeIaqb2EuyjA9x1-M7MRn_zKzzDM",
+  TOKEN: "",
   language: "en",
-  ownerID: ["962994407651553302", ""], 
+  ownerID: ["", ""], 
   mongodbUri : "mongodb+srv://shiva:shiva@musicbotyt.ouljywv.mongodb.net/?retryWrites=true&w=majority",
   spotifyClientId : "d92baed9605a45a39ed7c2a2d960b1c1",
   spotifyClientSecret : "e9b29f6739de4315bc03b6d8a8e93b03",
